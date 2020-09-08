0fb8d518b4b8c61efd4f52318908116b3eab4307
Sreeram225 <68415063+Sreeram225@users.noreply.github.com>
2020-09-08 12:08:19 +0530

    Update contiguous array.py

--- contiguous array.py
+++ contiguous array.py
@@ -1,9 +1,12 @@
-list1=[1,2,3,4,5]
+list1=[1, 2, 3, -2, 5]
 sublist=[[]]
+cur_sum=[]
+dict1={}
 for i in range(len(list1)+1):
   for j in range(i+1,len(list1)+1):
     sub=list1[i:j]
     sublist.append(sub)
-    print(sub)
     x=sum(sub)
-    print(x)
+    print(sub,         "sum ", x)
+    cur_sum.append(x)
+print("maximum contiguous array sum is ",max(cur_sum))
